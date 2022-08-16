88b508c4a20b172cbdd90f14e199de05d8c4ec45
Carson <carson.alberding@cdpipelinedevshop.com>
2022-08-16 09:29:51 -0400

    commit 2

---
+++ src/ProblemSet2_2.java
@@ -0,0 +1,17 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        System.out.println(" _    _      _ _ ");
+        System.out.println(" | |  | |    | | |      ");
+        System.out.println(" | |  | |    | | |      ");
+        System.out.println(" | |  | |      | || |       ");
+        System.out.println(" | |__| |  ___ | || |  ___  ");
+        System.out.println(" |  __  | / _ \\| || | / _ \\ ");
+        System.out.println(" | |  | ||  __/| || || (_) |");
+        System.out.println(" |_|  |_| \\___||_||_| \\___/ ");
+        
+        
+        
+    } 
+}
